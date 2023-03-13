c909be6ebef2f934a66ad37ef55dc106aea61136
asadrizv <asadimranrizvi9@gmail.com>
2023-03-13 02:47:42 +0100

    Made filename configurable

---
+++ src/src/csv_parser.rs
@@ -0,0 +1,138 @@
+use std::collections::HashMap;
+use std::fs::File;
+
+use csv;
+
+use crate::error::{AppError, CsvParserError, FormulaEvaluationErrorEnum};
+use crate::error::AppError::FormulaEvaluationError;
+use crate::formula_evaluator::EvaluatedFormula;
+
+#[derive(Debug)]
+pub struct CsvData {
+    pub rows: Vec<CsvRow>,
+    pub columns: HashMap<String, Vec<EvaluatedFormula>>,
+    pub fee: f64,
+    pub cost_threshold: f64,
+}
+
+#[derive(Debug)]
+pub struct CsvRow {
+    pub row_number: usize,
+    pub formulas: Vec<String>,
+}
+
+pub enum Formula {
+    Literal(f64),
+    Reference(String),
+}
+
+pub fn parse_formula(formula_str: &str) -> Result<Formula, AppError> {
+    if let Ok(value) = formula_str.parse::<f64>() {
+        return Ok(Formula::Literal(value));
+    }
+
+    if let Some('@') = formula_str.chars().find(|c| !c.is_numeric()) {
+        return Ok(Formula::Reference(formula_str.to_string()));
+    }
+
+    Err(FormulaEvaluationError(
+        FormulaEvaluationErrorEnum::InvalidFormula(format!(
+            "Invalid formula: {}",
+            formula_str
+        )),
+    ))
+}
+
+pub fn parse_csv_file(filename: &str) -> Result<CsvData, AppError> {
+    let file = File::open(filename).map_err(|err| AppError::CsvParsingError(CsvParserError::IoError(err)))?;
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .trim(csv::Trim::All)
+        .from_reader(file);
+
+    let mut rows = vec![];
+    let mut columns = HashMap::new();
+    let mut fee = 0.0;
+    let mut cost_threshold = 0.0;
+    let mut prev_fields: Option<usize> = None;
+
+    for (i, result) in reader.records().enumerate() {
+        let record = match result {
+            Ok(r) => r,
+            Err(_) => {
+                // Skip over records that couldn't be parsed
+                continue;
+            }
+        };
+
+        if record[0].starts_with('!') {
+            if record[0].to_string() == "!fee" {
+                fee = record[1]
+                    .parse::<f64>()
+                    .map_err(|err| FormulaEvaluationError(FormulaEvaluationErrorEnum::InvalidFormula(format!("Error parsing float: {}", err))))?;
+            } else if record[0].to_string() == "!cost_threshold" {
+                cost_threshold = record[1]
+                    .parse::<f64>()
+                    .map_err(|err| FormulaEvaluationError(FormulaEvaluationErrorEnum::InvalidFormula(format!("Error parsing float: {}", err))))?;
+            }
+        } else {
+            let mut formulas: Vec<String> = vec![];
+            let mut tokens: Vec<String> = vec![];
+
+            if record.len() < 3 {
+                continue;
+            }
+
+            if let Some(pf) = prev_fields {
+                if record.len() != pf {
+                    // Skip over records with different number of fields than the previous record
+                    continue;
+                }
+            }
+
+            for (j, cell) in record[2].split(',').enumerate() {
+                let column_label = format!("{}_{}", j + 1, record[0].to_string());
+
+                if cell.starts_with('=') {
+                    formulas.push(cell[1..].to_string());
+                    tokens.push(column_label.clone());
+                } else {
+                    let value = cell.parse::<f64>().ok();
+
+                    columns
+                        .entry(column_label.clone())
+                        .or_insert_with(|| vec![])
+                        .push(EvaluatedFormula {
+                            name: format!("{}_value", column_label),
+                            value,
+                        });
+                }
+            }
+
+            rows.push(CsvRow {
+                row_number: i,
+                formulas,
+            });
+
+            for token in tokens {
+                columns
+                    .entry(token.clone())
+                    .or_insert_with(|| vec![])
+                    .push(EvaluatedFormula {
+                        name: format!("{}_price", token),
+                        value: None,
+                    });
+            }
+
+            prev_fields = Some(record.len());
+        }
+    }
+
+    Ok(CsvData {
+        rows,
+        columns,
+        fee,
+        cost_threshold,
+    })
+}
+
---
+++ src/src/error.rs
@@ -0,0 +1,91 @@
+use std::fmt;
+use std::num::ParseIntError;
+
+// Define custom error types
+#[derive(Debug)]
+pub enum CsvParserError {
+    IoError(std::io::Error),
+}
+
+impl fmt::Display for CsvParserError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CsvParserError::IoError(err) => write!(f, "I/O error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for CsvParserError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            CsvParserError::IoError(err) => Some(err),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum FormulaEvaluationErrorEnum {
+    InvalidFormula(String),
+}
+
+impl fmt::Display for FormulaEvaluationErrorEnum {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FormulaEvaluationErrorEnum::InvalidFormula(var) => {
+                write!(f, "Invalid formula: {}", var)
+            }
+        }
+    }
+}
+
+impl std::error::Error for FormulaEvaluationErrorEnum {}
+
+#[derive(Debug)]
+pub enum AppError {
+    CsvParsingError(CsvParserError),
+    FormulaEvaluationError(FormulaEvaluationErrorEnum),
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AppError::CsvParsingError(err) => write!(f, "CSV parsing error: {}", err),
+            AppError::FormulaEvaluationError(err) => {
+                write!(f, "Formula evaluation error: {}", err)
+            }
+        }
+    }
+}
+
+impl std::error::Error for AppError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            AppError::CsvParsingError(err) => Some(err),
+            AppError::FormulaEvaluationError(err) => Some(err),
+        }
+    }
+}
+
+impl From<std::io::Error> for CsvParserError {
+    fn from(err: std::io::Error) -> Self {
+        CsvParserError::IoError(err)
+    }
+}
+
+impl From<CsvParserError> for AppError {
+    fn from(error: CsvParserError) -> Self {
+        AppError::CsvParsingError(error)
+    }
+}
+
+impl From<FormulaEvaluationErrorEnum> for AppError {
+    fn from(error: FormulaEvaluationErrorEnum) -> Self {
+        AppError::FormulaEvaluationError(error)
+    }
+}
+
+impl From<ParseIntError> for AppError {
+    fn from(error: ParseIntError) -> Self {
+        AppError::FormulaEvaluationError(FormulaEvaluationErrorEnum::InvalidFormula(format!("Error parsing integer: {}", error)))
+    }
+}
